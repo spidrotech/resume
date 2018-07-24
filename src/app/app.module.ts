76f3b9fce682b61c9c3bd5d30dbf34da2ff2cf2d
hafamri <hafamri@WKWFR2015189.global.publicisgroupe.net>
2018-07-24 11:16:17 +0200

    Add resume bootstrap template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlertModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AlertModule.forRoot(),
     BrowserModule
   ],
   providers: [],
