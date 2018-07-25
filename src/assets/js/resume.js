232076ba2c0b6d4d4cc8c7ff572a3c96048945ae
hafamri <hafamri@WKWFR2015189.global.publicisgroupe.net>
2018-07-25 18:00:05 +0200

    update index

--- src/assets/js/resume.js
+++ src/assets/js/resume.js
@@ -1,8 +1,8 @@
-(function($) {
-  "use strict"; // Start of use strict
+//(function($) {
+//  "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -18,7 +18,6 @@
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
-    alert('ici');
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
@@ -26,4 +25,4 @@
     target: '#sideNav'
   });
 
-})(jQuery); // End of use strict
+//})(jQuery); // End of use strict
